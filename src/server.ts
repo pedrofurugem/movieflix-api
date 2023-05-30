9ec9f9622b5334bee73b48540d59c7bd06e97795
Pedro Roberto <pedrofuruguem@hotmail.com>
2023-05-30 16:56:21 -0300

    adicionado a verificação de filmes duplicados

--- src/server.ts
+++ src/server.ts
@@ -23,7 +23,24 @@ app.get("/movies", async (_, res) => {
 app.post("/movies", async (req, res)=> {
     const { title, genre_id, language_id, oscar_count, 
         release_date } = req.body
+    
     try{
+        // case insensitive - se a busca for feita por john wick ou John wick ou JOHN WICK,
+        //o registro vai ser retornado na consulta
+
+        //case sensitive - se buscar por john wick e no banco estiver como John wick, não vai ser retornado na cosulta
+        const movieWithSameTitle = await prisma.movie.findFirst({
+            where: {
+                //titulo onde equals igual o titulo recebido da requisição
+                //modo é insensitivo
+                title: { equals: title, mode: "insensitive"}
+            }
+        })
+
+        if(movieWithSameTitle){
+            return res.status(409).send({message: "Ja existe um filme cadastrado com esse título"}) //409 se deu algum conflito
+        }
+
     await prisma.movie.create({
         data: {
         title,
